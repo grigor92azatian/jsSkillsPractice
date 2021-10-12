9fc91b3b4844420b7509e48e8fe98ef5bebd9f6b
Grigor Azatian <grigorazatian@yahoo.com>
2021-10-12 11:52:19 -0700

    basic layout of buttons, carousel, and dot container completed

--- image-carousel-script.js
+++ image-carousel-script.js
@@ -1,5 +1,2 @@
-//super wide div that will hold the image slides will slide left to right by adjusting the left/right margin
-//scrollbar will remain hidden
-//need to add a "picture" frame in front of the image slide container and have only one image show at a time
-
-console.log("testing");
+//not going to be a true image slider
+//change display from "none" to "block" in order to reveal the image needed
